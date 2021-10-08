3e5d44ec508a3e3cfdad4015f5f0ed9ef03f844e
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-10-08 08:38:21 +0200

    Added iteration

--- src/colls/Main.java
+++ src/colls/Main.java
@@ -11,5 +11,16 @@ public class Main {
         list.put("C", 3);
         list.put("D", 4);
         list.put("E", 5);
+
+        // #1
+        for (Map.Entry<String, Integer> entry : list.entrySet()){
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+
+        // #2
+        System.out.println(list.toString());
+
+        // #3
+        list.forEach((key, value) -> System.out.println(key + ": " + value));
     }
 }
