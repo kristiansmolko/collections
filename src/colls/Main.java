211c5c4d1323c3ee4bac73034e23e00e8c1a9150
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2021-10-08 08:19:48 +0200

    Initial commit

---
+++ src/colls/Main.java
@@ -0,0 +1,15 @@
+package colls;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main {
+    public static void main(String[] args) {
+        Map<String, Integer> list = new HashMap<>();
+        list.put("A", 1);
+        list.put("B", 2);
+        list.put("C", 3);
+        list.put("D", 4);
+        list.put("E", 5);
+    }
+}
